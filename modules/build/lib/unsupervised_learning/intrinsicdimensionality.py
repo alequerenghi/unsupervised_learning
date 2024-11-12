dc0cad93d91dbc1ab8b60ba1c674e2ece935623e
alequerenghi <querenghialessandro@icloud.com>
2024-11-12 14:12:45 +0100

    updated modules

--- modules/build/lib/unsupervised_learning/intrinsicdimensionality.py
+++ modules/build/lib/unsupervised_learning/intrinsicdimensionality.py
@@ -3,18 +3,13 @@ from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 
 
-class IntrinsicDimension():
+def two_nn(self, dataset: np.ndarray):
+    n = dataset.shape[0]
+    neigh = NearestNeighbors(n_neighbors=3)
+    neigh.fit(dataset)
+    data, _ = neigh.kneighbors(dataset)
+    mu = [0] * n
+    for i in range(n):
+        mu[i] = data[i, 2]/data[i, 1]
 
-    def __init__(self) -> None:
-        pass
-
-    def estimate(self, dataset: np.ndarray):
-        n = dataset.shape[0]
-        neigh = NearestNeighbors(n_neighbors=3)
-        neigh.fit(dataset)
-        data, _ = neigh.kneighbors(dataset)
-        mu = [0] * n
-        for i in range(n):
-            mu[i] = data[i, 2]/data[i, 1]
-
-        return n / sum(np.log(mu))
+    return n / sum(np.log(mu))
--- modules/build/lib/unsupervised_learning/kernelpca.py
+++ modules/build/lib/unsupervised_learning/kernelpca.py
@@ -1,6 +1,73 @@
 from typing import Literal
-from unsupervised_learning import build_kernel, double_center
+# from unsupervised_learning import build_kernel, double_center
 import numpy as np
+from numba import njit, prange
+
+
+def build_kernel(dataset, kernel, degree, gamma):
+    match kernel:
+        case 'poly':
+            return poly_kernel(dataset, degree, gamma)
+        case 'gauss':
+            return gauss_kernel(dataset, gamma)
+        case _:
+            return linear_kernel(dataset)
+
+
+@njit(parallel=True)
+def poly_kernel(dataset, degree, gamma):
+    n = dataset.shape[0]
+    kernel = np.zeros((n, n))
+    for i in prange(n):
+        for j in range(i, n):
+            kernel[i, j] = (dataset[i] @ dataset[j] + gamma) ** degree
+            if i-j:
+                kernel[j, i] = kernel[i, j]
+    return kernel
+
+
+@njit(parallel=True)
+def linear_kernel(dataset):
+    n = dataset.shape[0]
+    kernel = np.zeros((n, n))
+    for i in prange(n):
+        for j in range(i, n):
+            kernel[i, j] = dataset[i] @ dataset[j]
+            if i-j:
+                kernel[j, i] = kernel[i, j]
+    return kernel
+
+
+@njit(parallel=True)
+def gauss_kernel(dataset, gamma):
+    n = dataset.shape[0]
+    kernel = np.zeros((n, n))
+    for i in prange(n):
+        for j in range(i, n):
+            kernel[i, j] = np.exp(-sum(
+                (dataset[i] - dataset[j])**2) / (2 * gamma ** 2))
+            if i-j:
+                kernel[j, i] = kernel[i, j]
+    return kernel
+
+
+@njit(parallel=True)
+def double_center(kernel):
+    n = kernel.shape[0]
+    gram = np.zeros((n, n))
+    row_sum = np.zeros(n)
+    for i in prange(n):
+        temp = 0
+        for k in range(n):
+            temp += kernel[i]@kernel[k]
+        row_sum[i] = temp
+    for i in prange(n):
+        for j in range(i, n):
+            gram[i, j] = kernel[i]@kernel[j] - 1/n * row_sum[i] - \
+                1/n * row_sum[j] + 1/n**2 * sum(row_sum)
+            if i-j:
+                gram[j, i] = gram[i, j]
+    return gram
 
 
 class KernelPCA():
