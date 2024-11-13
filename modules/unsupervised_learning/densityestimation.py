d0775ef781c8e623f96635bb03e76dea1f46e6d2
alequerenghi <117732128+alequerenghi@users.noreply.github.com>
2024-11-13 17:14:33 +0100

    corrected incorrect histogram method

--- modules/unsupervised_learning/densityestimation.py
+++ modules/unsupervised_learning/densityestimation.py
@@ -4,14 +4,15 @@ from numba import prange, njit
 
 @njit
 def histograms(X: np.ndarray, bins=None):
+    X = X - min(X)
     n = X.shape[0]
     if bins is None:
         IQR = np.quantile(X, 0.75) - np.quantile(X, 0.25)
         delta = 2 * IQR / np.cbrt(n)
-        bins = int((max(X) - min(X)) / delta)
+        bins = int(max(X) / (delta-1))
     else:
-        delta = (max(X)-min(X)+1)/bins
-    X = (X-min(X)) / delta
+        delta = max(X)/(bins-1)
+    X = X / delta
     histo = np.zeros(bins)
     for i in X:
         histo[int(i)] += 1
