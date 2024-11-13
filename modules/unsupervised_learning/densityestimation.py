24810c1c464b7e4c7b420f1d77e7047df3840abc
alequerenghi <querenghialessandro@icloud.com>
2024-11-13 16:39:52 +0100

    updated density estimation methods

--- modules/unsupervised_learning/densityestimation.py
+++ modules/unsupervised_learning/densityestimation.py
@@ -2,27 +2,49 @@ import numpy as np
 from numba import prange, njit
 
 
-@njit(parallel=True)
-def histograms(dataset: np.ndarray):
-    n = dataset.shape[0]
-    IQR = np.quantile(dataset, 0.75) - np.quantile(dataset, 0.25)
-    delta = 2 * IQR / np.cbrt(n)
-    dataset = dataset / delta
-    histo = np.zeros(int(dataset.max/delta)+1)
-    for i in dataset:
-        histo[int(i/delta)] += 1
+@njit
+def histograms(X: np.ndarray, bins=None):
+    n = X.shape[0]
+    if bins is None:
+        IQR = np.quantile(X, 0.75) - np.quantile(X, 0.25)
+        delta = 2 * IQR / np.cbrt(n)
+        bins = int((max(X) - min(X)) / delta)
+    else:
+        delta = (max(X)-min(X)+1)/bins
+    X = (X-min(X)) / delta
+    histo = np.zeros(bins)
+    for i in X:
+        histo[int(i)] += 1
     return histo / n
 
 
 @njit(parallel=True)
-def kde(dataset: np.ndarray):
-    n = dataset.shape[0]
-    h = 0.9 * min(dataset.std() ** 2, np.quantile(dataset, 0.75) -
-                  np.quantile(dataset, 0.25)/1.34) * n ** (- 1/5)
-    rho = np.zeros(n)
-    for i in prange(n):
+def gaussian_kernel_density(Y: np.ndarray, X: np.ndarray, bandwidth):
+    # sigma = np.std(X)
+    density = np.zeros(X.shape[0])
+    for i in prange(X.shape[0]):
         kernel = 0
-        for j in range(i, n):
-            kernel += np.exp(-(((dataset[i] - dataset[j])/h) ** 2) / 2)
-        rho[i] = kernel / np.sqrt(2*np.pi) / n / h
-    return rho
+        for j in range(Y.shape[0]):
+            kernel += np.exp(-(((X[i] - Y[j])/(bandwidth*2)) ** 2))
+        density[i] = kernel / (np.sqrt(2*np.pi)*X.shape[0]*bandwidth)
+    return density
+
+
+class KernelDensity:
+    def __init__(self, kernel='gaussian', bandwidth=None) -> None:
+        self.bandwidth = bandwidth
+        self.kernel = kernel
+
+    def fit(self, X: np.ndarray):
+        self.X = X
+        self.n = self.X.shape[0]
+        if self.bandwidth is None:
+            IQR = np.quantile(self.X, 0.75) - \
+                np.quantile(self.X, 0.25)
+            self.bandwidth = 0.9 * \
+                min(self.X.std()**2, IQR/1.34)*self.n**(-1/5)
+        return self
+
+    def score_sample(self, X):
+        # if self.kernel is 'gaussian':
+        return gaussian_kernel_density(self.X, X, self.bandwidth)
