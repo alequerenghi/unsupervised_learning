6b1b46e5aa72922d6908e54e1eced72d7fe2d7a6
alequerenghi <querenghialessandro@icloud.com>
2024-11-14 16:47:50 +0100

    completed density estimation

--- modules/unsupervised_learning/densityestimation.py
+++ modules/unsupervised_learning/densityestimation.py
@@ -3,13 +3,13 @@ from numba import prange, njit
 
 
 @njit
-def histograms(X: np.ndarray, bins=None):
+def histograms(X: np.ndarray, bins=0):
     X = X - min(X)
     n = X.shape[0]
-    if bins is None:
+    if bins is 0:
         IQR = np.quantile(X, 0.75) - np.quantile(X, 0.25)
         delta = 2 * IQR / np.cbrt(n)
-        bins = int(max(X) / delta)+ 1
+        bins = int(max(X) / delta) + 1
     else:
         delta = max(X)/(bins-1)
     X = X / delta
