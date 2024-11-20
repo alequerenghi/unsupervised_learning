2f2f70ccd292ec9d8f75842509ff7f6f418f1d34
alequerenghi <117732128+alequerenghi@users.noreply.github.com>
2024-11-20 17:53:45 +0100

    Updated kmedoids++ to use numba

--- modules/unsupervised_learning/clustering/clustering.py
+++ modules/unsupervised_learning/clustering/clustering.py
@@ -1,17 +1,18 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+from numba import njit, int64
 
-
+@njit
 def kmeans_plus_plus(X: np.ndarray, k: int):
-    centers = np.zeros(k, dtype=int)
-    d = np.zeros(X.shape[0], dtype=int)
+    centers = np.zeros(k, dtype=int64)
+    d = np.zeros(X.shape[0], dtype=int64)
 
     centers[0] = np.random.randint(low=X.shape[0])
     # until all clusters are added
     for cluster in range(k):
         distances = NearestNeighbors(n_neighbors=1).fit(
             X[centers[:cluster+1]]).kneighbors(X)[0].flatten()
-        distances.astype(int)
+        distances.astype(int64)
         centers[cluster] = np.argmax(np.random.randint(distances**2+1))
     return centers
     for idx, node in enumerate(X):
@@ -20,6 +21,6 @@ def kmeans_plus_plus(X: np.ndarray, k: int):
         for j in range(cluster+1):
             closest = min(
                 abs(np.sum(node - X[centers[j]])), closest)
-        d[idx] = closest
-    centers[cluster] = np.argmax(np.random.randint(d+1))
+        d[idx] = np.randint(closest+1)
+    centers[cluster] = np.argmax(d)
     return centers
