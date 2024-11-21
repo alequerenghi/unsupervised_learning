36b4439b862c581999cc895bcf49d4817fa3f2cc
alequerenghi <querenghialessandro@icloud.com>
2024-11-21 13:39:26 +0100

    modified kmeans++

--- modules/unsupervised_learning/clustering/clustering.py
+++ modules/unsupervised_learning/clustering/clustering.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from numba import njit, int64
 
+
 @njit
 def kmeans_plus_plus(X: np.ndarray, k: int):
     centers = np.zeros(k, dtype=int64)
@@ -13,14 +14,7 @@ def kmeans_plus_plus(X: np.ndarray, k: int):
         distances = NearestNeighbors(n_neighbors=1).fit(
             X[centers[:cluster+1]]).kneighbors(X)[0].flatten()
         distances.astype(int64)
-        centers[cluster] = np.argmax(np.random.randint(distances**2+1))
-    return centers
-    for idx, node in enumerate(X):
-        closest = np.inf
-        # find closest centroid
-        for j in range(cluster+1):
-            closest = min(
-                abs(np.sum(node - X[centers[j]])), closest)
-        d[idx] = np.randint(closest+1)
-    centers[cluster] = np.argmax(d)
+        for idx, val in enumerate(distances):
+            d[idx] = np.random.randint(val**2+1)
+        centers[cluster] = np.argmax(d)
     return centers
